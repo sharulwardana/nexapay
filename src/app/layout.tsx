c8c7a4a4cef6203b7e6886a8506c288cfb1e658d
sharulwardana <sharulwrdn10@gmail.com>
2026-08-21 23:41:22 +0700

    fix: disable mobile and tablet viewport auto-zoom permanently

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,8 @@ export const viewport: Viewport = {
   ],
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 5,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
